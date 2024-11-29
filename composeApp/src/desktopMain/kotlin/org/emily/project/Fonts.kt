d335c4f89d519fdc6845c8e1d0ac23d528bfd27a
emilygofeld <emiloren17@gmail.com>
2024-11-29 22:59:01 +0200

    added montserrat font

---
+++ composeApp/src/desktopMain/kotlin/org/emily/project/Fonts.kt
@@ -0,0 +1,91 @@
+package org.emily.project
+
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.platform.Font
+
+object Fonts {
+    val montserratFontFamily = FontFamily(
+        Font(
+            "fonts/montserrat_black.ttf",
+            FontWeight.Black
+        ),
+        Font(
+            "fonts/montserrat_blackitalic.ttf",
+            FontWeight.Black, style = FontStyle.Italic
+        ),
+        Font(
+            "fonts/montserrat_bold.ttf",
+            FontWeight.Bold
+        ),
+        Font(
+            "fonts/montserrat_bolditalic.ttf",
+            FontWeight.Bold, style = FontStyle.Italic
+        ),
+        Font(
+            "fonts/montserrat_extrabold.ttf",
+            FontWeight.ExtraBold
+        ),
+        Font(
+            "fonts/montserrat_extrabolditalic.ttf",
+            FontWeight.ExtraBold, style = FontStyle.Italic
+        ),
+        Font(
+            "fonts/montserrat_extralight.ttf",
+            FontWeight.ExtraLight
+        ),
+        Font(
+            "fonts/montserrat_extralightitalic.ttf",
+            FontWeight.ExtraLight, style = FontStyle.Italic
+        ),
+        Font(
+            "fonts/montserrat_italic_variablefont_wght.ttf",
+            FontWeight.Normal, style = FontStyle.Italic
+        ),
+        Font(
+            "fonts/montserrat_italic.ttf",
+            FontWeight.Normal, style = FontStyle.Italic
+        ),
+        Font(
+            "fonts/montserrat_light.ttf",
+            FontWeight.Light
+        ),
+        Font(
+            "fonts/montserrat_lightitalic.ttf",
+            FontWeight.Light, style = FontStyle.Italic
+        ),
+        Font(
+            "fonts/montserrat_medium.ttf",
+            FontWeight.Medium
+        ),
+        Font(
+            "fonts/montserrat_mediumitalic.ttf",
+            FontWeight.Medium, style = FontStyle.Italic
+        ),
+        Font(
+            "fonts/montserrat_regular.ttf",
+            FontWeight.Normal
+        ),
+        Font(
+            "fonts/montserrat_semibold.ttf",
+            FontWeight.SemiBold
+        ),
+        Font(
+            "fonts/montserrat_semibolditalic.ttf",
+            FontWeight.SemiBold, style = FontStyle.Italic
+        ),
+        Font(
+            "fonts/montserrat_thin.ttf",
+            FontWeight.Thin
+        ),
+        Font(
+            "fonts/montserrat_thinitalic.ttf",
+            FontWeight.Thin, style = FontStyle.Italic
+        ),
+        Font(
+            "fonts/montserrat_variablefont_wght.ttf",
+            FontWeight.Normal
+        )
+    )
+}
